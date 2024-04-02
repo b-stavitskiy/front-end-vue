a727b4ef85f47a2955dfb1f4b6be6d5a10f670af
Evgeniy Galkovskiy <zhegal@gmail.com>
2024-04-02 09:26:10 +0300

    Change statuses

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="container is-widescreen" style="margin-top: 15px;">
-    <vue-table class="box is-paddingless is-rounded"
+  <div class="container is-widescreen" style="margin-top: 15px">
+    <vue-table
+      class="box is-paddingless is-rounded"
       path="https://test.1234.lc/api/testData/initTable"
-      id="example"/>
+      id="example"
+    >
+      <template v-slot:status_id="{ row, column }">
+        <span>{{ replaceStatus(row[column.name]) }}</span>
+      </template>
+    </vue-table>
   </div>
 </template>
 
 <script>
 import { VueTable } from '@enso-ui/tables/bulma';
+import { statuses } from '../static/statuses';
 
 export default {
   name: 'Home',
   components: {
     VueTable,
+  },
+  methods: {
+    replaceStatus(id) {
+      return statuses[id] ?? null;
+    },
   },
 };
 </script>
---
+++ src/static/statuses.js
@@ -0,0 +1,7 @@
+export const statuses = {
+  1: 'Pending',
+  2: 'In Progress',
+  3: 'Completed',
+  4: 'On Hold',
+  5: 'Cancelled',
+};
